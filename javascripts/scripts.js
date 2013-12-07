9430d1ef890d277fd0118110d6d357ee507068ac
rcrespo9 <rudycrespo@hushmail.com>
2013-12-06 22:29:29 -0500

    implemented bootstrap navbar and it is now responsive

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -17,35 +17,6 @@ jQuery(document).ready(function ($) {
     mywindow = $(window);
     htmlbody = $('html,body');
 
-
-    //Setup waypoints plugin
-    slide.waypoint(function (event, direction) {
-
-        //cache the variable of the data-slide attribute associated with each slide
-        dataslide = $(this).attr('data-slide');
-
-        //If the user scrolls up change the navigation link that has the same data-slide attribute as the slide to active and 
-        //remove the active class from the previous navigation link 
-        if (direction === 'down') {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').prev().removeClass('active');
-        }
-        // else If the user scrolls down change the navigation link that has the same data-slide attribute as the slide to active and 
-        //remove the active class from the next navigation link 
-        else {
-            $('.navigation li[data-slide="' + dataslide + '"]').addClass('active').next().removeClass('active');
-        }
-
-    });
-
-    //waypoints doesnt detect the first slide when user scrolls back up to the top so we add this little bit of code, that removes the class 
-    //from navigation link slide 2 and adds it to navigation link slide 1. 
-    mywindow.scroll(function () {
-        if (mywindow.scrollTop() == 0) {
-            $('.navigation li[data-slide="1"]').addClass('active');
-            $('.navigation li[data-slide="2"]').removeClass('active');
-        }
-    });
-
     //Create a function that will be passed a slide number and then will scroll to that slide using jquerys animate. The Jquery
     //easing plugin is also used, so we passed in the easing method of 'easeInOutQuint' which is available throught the plugin.
     function goToByScroll(dataslide) {
@@ -97,13 +68,13 @@ $('#ca-container').contentcarousel({
 });
 
 
-// fadeIn navbar
+// fadein navbar
 $(window).scroll(function() {
     if ($(this).scrollTop() > 500) {
-        $('.nav').fadeIn();
+        $('.navbar').fadeIn(800);
     } else {
-        $('.nav').fadeOut();
+        $('.navbar').fadeOut();
     }
 });
 
-$('.navigation').hide();
+$('.navbar').hide();
