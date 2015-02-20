c753ae3d92da1d26ba5f222cd523c7155c2ee164
Rudy Crespo <rudy.crespo20@gmail.com>
2015-02-19 22:35:21 -0500

    particle ground

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,57 @@
+'use strict';
+
 var app = app || {};
+
+app.particleEffect = (function() {
+	var s;
+
+	return {
+		
+		settings: {
+			theBody: $('body'),
+			introLine: $('.intro-line')
+		},
+
+		init: function() {
+			s = this.settings;
+			this.startParticles();
+		},
+
+		whichAnimationEvent: function() {
+			var t;
+			var el = document.createElement('fakeelement');
+
+			var animations = {
+			    'animation'      : 'animationend',
+			    'OAnimation'     : 'oAnimationEnd',
+			    'MozAnimation'   : 'animationend',
+			    'WebkitAnimation' : 'webkitAnimationEnd'
+			};
+
+			for (t in animations){
+				if (el.style[t] !== undefined){
+					return animations[t];
+				}
+			}
+		},
+
+		startParticles: function() {
+			var animationEvent = this.whichAnimationEvent();
+
+			s.introLine.one(animationEvent, function() {
+
+				s.theBody.particleground({
+				    dotColor: 'rgba(255,255,255,.4)',
+				    lineColor: 'rgba(255,255,255,.4)'
+				});
+
+			});
+		}
+
+	};
+
+}());
+
+$(function() {
+	app.particleEffect.init();
+});
