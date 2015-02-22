0737711af58131614ae3165762eeb510d74bf518
Rudy Crespo <rudy.crespo20@gmail.com>
2015-02-22 00:24:21 -0500

    cname gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -350,6 +350,9 @@ module.exports = function (grunt) {
           src: 'node_modules/apache-server-configs/dist/.htaccess',
           dest: '<%= config.dist %>/.htaccess'
         }, {
+        src: '<%= config.app %>/CNAME',
+        dest: '<%= config.dist %>/CNAME'
+        }, {
           expand: true,
           dot: true,
           cwd: '.',
