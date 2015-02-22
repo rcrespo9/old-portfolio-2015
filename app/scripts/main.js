09405df9319f0f0d7ad9337bd7aa0cf58b5aab75
Rudy Crespo <rudy.crespo20@gmail.com>
2015-02-21 23:15:29 -0500

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var app = app || {};
+
+app.particleEffect = (function() {
+	var s;
+
+	return {
+		
+		settings: {
+			theBody: $('body'),
+			introLine: $('.intro-line')
+		},
+
+		init: function() {
+			s = this.settings;
+			this.startParticles();
+		},
+
+		whichAnimationEvent: function() {
+			var t;
+			var el = document.createElement('fakeelement');
+
+			var animations = {
+			    'animation'      : 'animationend',
+			    'OAnimation'     : 'oAnimationEnd',
+			    'MozAnimation'   : 'animationend',
+			    'WebkitAnimation' : 'webkitAnimationEnd'
+			};
+
+			for (t in animations){
+				if (el.style[t] !== undefined){
+					return animations[t];
+				}
+			}
+		},
+
+		startParticles: function() {
+			var animationEvent = this.whichAnimationEvent();
+			var particleColor = '#34495e';
+
+			s.introLine.one(animationEvent, function() {
+
+				s.theBody.particleground({
+				    dotColor: particleColor,
+				    lineColor: particleColor,
+				    density: 16000
+				});
+
+			});
+		}
+
+	};
+
+}());
+
+$(function() {
+	app.particleEffect.init();
+});
